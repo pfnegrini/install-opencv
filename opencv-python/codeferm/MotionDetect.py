2fbf15ab6bd2c9d5f04b704b4e6fa3546e1f0e04
Steven P. Goldsmith <sgjava@gmail.com>
2015-04-04 12:37:14 -0400

    Added main

--- opencv-python/codeferm/MotionDetect.py
+++ opencv-python/codeferm/MotionDetect.py
@@ -31,67 +31,68 @@ def contours(source):
         movementLocations.append(rect)
     return movementLocations
 
-# Configure logger
-logger = logging.getLogger("MotionDetect")
-logger.setLevel("INFO")
-formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(module)s %(message)s")
-handler = logging.StreamHandler(sys.stdout)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-# If no args passed then default to internal file
-if len(sys.argv) < 2:
-    url = "../../resources/traffic.mp4"
-else:
-    url = sys.argv[1]
-outputFile = "../../output/motion-detect-python.avi"
-videoCapture = cv2.VideoCapture(url)
-logger.info("OpenCV %s" % cv2.__version__)
-logger.info("Input file: %s" % url)
-logger.info("Output file: %s" % outputFile)
-logger.info("Resolution: %dx%d" % (videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH),
-                               videoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-videoWriter = cv2.VideoWriter(outputFile, cv2.VideoWriter_fourcc(*'DIVX'), videoCapture.get(cv2.CAP_PROP_FPS),
-                              (int(videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(videoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT))), True)
-lastFrame = False
-frames = 0
-framesWithMotion = 0
-movingAvgImg = None
-totalPixels = videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH) * videoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)
-movementLocations = []
-start = time.time()
-while not lastFrame:
-    ret, image = videoCapture.read()
-    if ret:
-        # Generate work image by blurring
-        workImg = cv2.blur(image, (8, 8))
-        # Generate moving average image if needed
-        if movingAvgImg == None:
-            movingAvgImg = numpy.float32(workImg)
-        # Generate moving average image
-        cv2.accumulateWeighted(workImg, movingAvgImg, .03)
-        diffImg = cv2.absdiff(workImg, cv2.convertScaleAbs(movingAvgImg))
-        # Convert to grayscale
-        grayImg = cv2.cvtColor(diffImg, cv2.COLOR_BGR2GRAY)
-        # Convert to BW
-        return_val, grayImg = cv2.threshold(grayImg, 25, 255, cv2.THRESH_BINARY)
-        # Total number of changed motion pixels
-        motionPercent = 100.0 * cv2.countNonZero(grayImg) / totalPixels
-        # Detect if camera is adjusting and reset reference if more than maxChange
-        if motionPercent > 25.0:
-            movingAvgImg = numpy.float32(workImg)
-        movementLocations = contours(grayImg)
-        # Threshold trigger motion
-        if motionPercent > 0.75:
-            framesWithMotion += 1
-            for x, y, w, h in movementLocations:
-                # Draw rectangle around fond object
-                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        videoWriter.write(image)
-        frames += 1
+if __name__ == '__main__':
+    # Configure logger
+    logger = logging.getLogger("MotionDetect")
+    logger.setLevel("INFO")
+    formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(module)s %(message)s")
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    # If no args passed then default to internal file
+    if len(sys.argv) < 2:
+        url = "../../resources/traffic.mp4"
     else:
-        lastFrame = True
-elapse = time.time() - start
-logger.info("%d frames, %d frames with motion" % (frames, framesWithMotion))
-logger.info("Elapse time: %4.2f seconds" % elapse)
-del videoCapture
-del videoWriter
+        url = sys.argv[1]
+    outputFile = "../../output/motion-detect-python.avi"
+    videoCapture = cv2.VideoCapture(url)
+    logger.info("OpenCV %s" % cv2.__version__)
+    logger.info("Input file: %s" % url)
+    logger.info("Output file: %s" % outputFile)
+    logger.info("Resolution: %dx%d" % (videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH),
+                                   videoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    videoWriter = cv2.VideoWriter(outputFile, cv2.VideoWriter_fourcc(*'DIVX'), videoCapture.get(cv2.CAP_PROP_FPS),
+                                  (int(videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(videoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT))), True)
+    lastFrame = False
+    frames = 0
+    framesWithMotion = 0
+    movingAvgImg = None
+    totalPixels = videoCapture.get(cv2.CAP_PROP_FRAME_WIDTH) * videoCapture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    movementLocations = []
+    start = time.time()
+    while not lastFrame:
+        ret, image = videoCapture.read()
+        if ret:
+            # Generate work image by blurring
+            workImg = cv2.blur(image, (8, 8))
+            # Generate moving average image if needed
+            if movingAvgImg == None:
+                movingAvgImg = numpy.float32(workImg)
+            # Generate moving average image
+            cv2.accumulateWeighted(workImg, movingAvgImg, .03)
+            diffImg = cv2.absdiff(workImg, cv2.convertScaleAbs(movingAvgImg))
+            # Convert to grayscale
+            grayImg = cv2.cvtColor(diffImg, cv2.COLOR_BGR2GRAY)
+            # Convert to BW
+            return_val, grayImg = cv2.threshold(grayImg, 25, 255, cv2.THRESH_BINARY)
+            # Total number of changed motion pixels
+            motionPercent = 100.0 * cv2.countNonZero(grayImg) / totalPixels
+            # Detect if camera is adjusting and reset reference if more than maxChange
+            if motionPercent > 25.0:
+                movingAvgImg = numpy.float32(workImg)
+            movementLocations = contours(grayImg)
+            # Threshold trigger motion
+            if motionPercent > 0.75:
+                framesWithMotion += 1
+                for x, y, w, h in movementLocations:
+                    # Draw rectangle around fond object
+                    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            videoWriter.write(image)
+            frames += 1
+        else:
+            lastFrame = True
+    elapse = time.time() - start
+    logger.info("%d frames, %d frames with motion" % (frames, framesWithMotion))
+    logger.info("Elapse time: %4.2f seconds" % elapse)
+    del videoCapture
+    del videoWriter
