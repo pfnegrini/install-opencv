34a81d813620c327dc682098ef1d068de39e49be
Steven P. Goldsmith <sgjava@gmail.com>
2015-09-18 18:42:57 -0400

    Added elapse time display

--- scripts/ubuntu/install-java.sh
+++ scripts/ubuntu/install-java.sh
@@ -132,3 +132,4 @@ ds=$((elapsedtimesec % 60))
 dm=$(((elapsedtimesec / 60) % 60))
 dh=$((elapsedtimesec / 3600))
 displaytime=$(printf "%02d:%02d:%02d" $dh $dm $ds)
+log "Elapse time: $displaytime"
